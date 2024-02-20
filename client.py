128ac214d0af8c506d9b95113dc49b8c9db9a557
kazwritesomething <kazumawada.nz@gmail.com>
2024-02-20 12:04:21 +1100

    commit

--- client.py
+++ client.py
@@ -1,108 +1,69 @@
+from email import message
 import socket
-import json  # 配列を送信するため
+import threading
+import json
 
-# サーバーのホストとポート
-server_host = '127.0.0.1'
-server_port = 9001
+# クライアントの設定
+HOST = '127.0.0.1'
+PORT = 5555
 
-# ソケットの作成
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
 
-# サーバーに接続
-client_socket.connect((server_host, server_port))
-
-# 送信の関数#
-
-
-def send_data(data):
-    client_socket.send(data.encode())
-# 受信の関数#
-
-
-def receive_data(buffer_size=1024):
-    return client_socket.recv(buffer_size).decode()
+# username,roomname,messageを入力してサーバーに送信
+# ユーザーから入力を受け取り、JSON形式に変換して送信
+# "roomA"と" roomA"でも一緒にする必要があるから、空欄を削除する必要がある。
+# username, roomname = input(
+#     "Enter your username, room name: or message ").split(',')
+user_input = input(
+    "Enter your username, room name: or message ").split(',')
+# 空欄を削除する必要がある。
 
 
-def receive_data_handler(hashmap):
-    # 送信した最後のコマンドの内容の処理が成功したかここで表示される
-    if "create" in hashmap.keys():
-        # {"create": message}
-        # "create"に対応する値を表示する
-        print(hashmap.get("create"))
-    elif "join" in hashmap.keys():
-        print(hashmap.get("join"))
-    elif "send" in hashmap.keys():
-        print(hashmap.get("send"))
-    elif "help" in hashmap.keys():
-        print(hashmap.get("help"))
-    elif "exit" in hashmap.keys():
-        print(hashmap.get("exit"))
-    else:
-        print("else")
+# ここをusername,roomname2つの場合、mesage1つの場合に分けてsendすればいいのか。
+# そしたら2回連続でusername,roomnameを提供した場合エラーを吐かせる必要がある。
+# サーバー側でも1つか2つかを判断させる必要がある。
+
+username = user_input[0].strip()
+roomname = user_input[1].strip()
+data = json.dumps([username, roomname])
+# ここを起点にsend()ロープに持っていく
+client.send(data.encode('utf-8'))
+# サーバーからメッセージを受信する関数
 
 
-while True:
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            # json.loads(message)
+            print(message)
+        except:
+            # サーバーとの接続が切れた場合
+            print("Connection lost.")
+            client.close()
+            break
 
-    # send
-    # こっちは送信と受信をし続けている。serverは受信と送信をし続けているか?
-    send_chat_message = input(
-
-        '/////////////////////////////////////////////////\
-    \n- create room->[room name,message-size,initial message to chatroom,create \
-    \n- join room->[room,user_name,join]\
-    \n- text to room->[message,send]\
-    \n- type"help"  if you want to know how this works:\
-    \n- exit\
-    \n/////////////////////////////////////////////////\
-    \n:')
-
-    send_chat_message_arr = send_chat_message.split(',')
-    send_data(json.dumps(send_chat_message_arr))
-
-    # receive
-    hashmap_data = receive_data()
-    hashmap = json.loads(hashmap_data)
-    if "create" in hashmap.keys() or "join" in hashmap.keys() or "send" in hashmap.keys():
-
-        receive_data_handler(hashmap)
-    else:
-        print("commmand is not valid")
-        break
-
-# close
-while True:
-    user_input = input("→ Enter 'exit' to close connection: ")
-    if user_input.lower() == "exit":
-        break
-client_socket.close()
+# ユーザーの入力をサーバーに送信する関数
 
 
-# 最初に{roomname}:{message-size}:{message}をサーバーに送信. messageは参加した全ての人に送信するメッセージ("welcome to war room" etc)
-# ↑送信者がすでにchatroomに参加している必要がある。
-# chatroom作成->自動的にそのユーザーがホストになる。(という事はユーザーClassも作成する必要がある)
-# chatroom作成->{"chatroom name" : ChatRoom(参加者,"chatroom name",別のハッシュマップ)}
-# ChatClient(クライアントのアドレス、ポート、その他の必要なデータ)
-# クライアントは一度に一部屋のみ参加可能
-# {アドレス&ポート : chatroom}で、ChatClientを特定する事ができる
-# {roomname}:joinで参加(その後にvalidationを通過したらmessageがそのユーザーに送られる)
-# メッセージをチャットルーム内に送信できる機能を作る。(forループで全員に同じメッセージを送るとか)
+# def send():
+#     while True:
+#         message = input()
+#         client.send(message.encode('utf-8'))
+def send():
+    while True:
+        # user_input = input("Enter your message: ")
+        user_input = input("")
+        # if user_input.lower() == 'exit':
+        #     break
+        message_data = [{"message": user_input}]
+        client.send(json.dumps(message_data).encode('utf-8'))
 
 
-# Online Chat Messenger
-# クライアントがチャットルームを作成し、参加することができるサービスを作成します。
-# 各チャットルームは一意のキーで識別されます。このキーはチャットルームの識別子として機能し、他のユーザーがそのチャットルームに参加することを可能にします。
-# 新しいチャットルームを作成する際には、タイトルと最大参加者数を指定する必要があります。
-# このサービスは、チャットルームの作成中にデータが失われないように、ポート A の TCP 接続を使用する必要があります。
-# チャットルームが作成されると、作成したクライアントは自動的に UDP でチャットルームに接続され、ホストとして指定されます。
-# チャットルームは、<string, ChatRoom> のハッシュマップとして実装する必要があり、ChatRoom は参加者、<string, ChatClient> オブジェクトの別のハッシュマップ、
-# サイズやタイトルなどのチャットルーム情報を含むオブジェクトとなります。
-# ChatClient は、クライアントのアドレスとポート、およびクライアントの追加データを含むオブジェクトです。
-# アドレスとポートを連結して ChatRoom のキーとすることで、ChatClient を一意に特定することができます。
-# 各クライアントは一度に 1 つのチャットルームにしか参加できず、チャットルームの参加者数はルーム作成時に指定した最大人数を超えることはできません。
-# サーバに送信するメッセージは、{roomname}:{message-size}:{message} というプレフィックスで始めます。roomname は参加した部屋を示す文字列、
-# message-size はメッセージのサイズ、message はチャットに参加した全ての人に送信するメッセージです。メッセージの送信時に、
-# 送信者がサーバ内のチャットルームに参加していることを確認する必要があります。
-
-# チャットルームに参加する場合は、{roomname}:join を含むメッセージをサーバに送信します。
-# メッセージをチャットルーム内の他の人に転送する場合は、単純な for ループを使うか、Python の asyncio ライブラリのような非同期ツールを使って
-# 転送することができます。
+# def send実行 マルチクライアントに対応
+send_thread = threading.Thread(target=send)
+send_thread.start()
+# def receive実行 マルチクライアントに対応
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
--- server.py
+++ server.py
@@ -1,194 +1,136 @@
-from email import message
 import socket
+import threading
 import json
-from unicodedata import name
-from urllib import response
 
-from playground import find_data  # 配列を送信するため
+# サーバーの設定
+HOST = '127.0.0.1'
+PORT = 5555
 
-# サーバーのホストとポート
-server_host = '0.0.0.0'
-server_port = 9001
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
 
-# ソケットの作成
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# サーバーのアドレスとポートにバインド
-server_socket.bind((server_host, server_port))
-
-# 接続の待機
-server_socket.listen(1)
-print('Waiting for incoming connections...')
-
-# クライアントからの接続を受け入れる
-client_socket, client_address = server_socket.accept()
-print(f'Connected to {client_address}')
-
-# 送信の関数#
+# 接続されたクライアントとユーザー名を格納する辞書
+clients = {}
+chatroom = {}
+rooms = {}
 
 
-def send_data(data):
-    client_socket.send(data.encode())
-# 受信の関数#
-
-
-def receive_data(buffer_size=1024):
-    return client_socket.recv(buffer_size).decode()
-
-
-class ChatRoom:
-    # ここのchatroomにどのユーザーがいるか知るために、
-    # chatroom class内にUserを入れた方が良いと思う。
+class Chatroom:
     def __init__(self, name):
         self.name = name
-        self.users = {}
-
-    def add_user(self, user):
-        # user.name->class User.nameから来ている。
-        self.users[user.name] = user
-        # self.users{admin:"admin"}
-
-    def get_user(self, name):
-        # hashmapのget
-        return self.users.get(name)
 
 
 class User:
     def __init__(self, name):
         self.name = name
 
-# # 新しいチャットルームを作成
-# python_chat = ChatRoom("Python Chat")
+# メッセージを受信し、他のクライアントに転送する関数
 
-# # ユーザーを作成
-# user1 = User("Alice")
-# user2 = User("Bob")
-
-# # ユーザーをチャットルームに追加
-# python_chat.add_user(user1)
-# python_chat.add_user(user2)
-
-# # チャットルームからユーザーを取得して表示
-# found_user = python_chat.get_user("Alice")
-# if found_user:
-#     print(f"User found: {found_user.username}")
-# else:
-#     print("User not found")
+# room:chatroomをインスタンス化する変数
+# rooms: chatroomたちを格納するhashmap
 
 
-def find_data(data):
-    file_path = "chatroom_data.txt"
+def handle_client(client_socket, addr):
+    try:
+        # ユーザー名をクライアントから受け取る
+        # 配列で受け取れるようにする{username,roomname,message}
+        ini_data_str = client_socket.recv(1024).decode('utf-8')
+        ini_data_arr = json.loads(ini_data_str)
+        ## 1回目のrecv##
+        # 新規chatroom
+        user = User(ini_data_arr[0])
+        room = Chatroom(ini_data_arr[1])
 
-    # テキストデータを読み取って、辞書形式に変換する
-    hashmap = {}
-    with open(file_path, "r") as file:
-        for line in file:
-            # 空白を削除
-            # 1行だからhashmapを1行にして書く必要がある
-            line = line.strip()
-            if line:
-                room, user, user_string = line.split(":")
-                # まだ空白があるらしいから削除
-                room = room.strip()
-                user = user.strip()
-                user_string = user_string.strip()
-                # 部屋が存在しなかったら辞書に登録
-                if room not in hashmap:
-                    hashmap[room] = {}
-                # key, value = user.strip()
-                key, value = user, user_string
-                hashmap[room][key] = value
-
-    # JSON形式に変換する
-    json_data = json.dumps(hashmap, indent=2)
-    json_data = json.loads(json_data)
-    # if json_data[room_name] == exit
-
-    # JSONデータを出力する
-    # print(json_data)
-    # print(json_data["good_room"]["admin"])  # {'admin': 'admin'}
-    if json_data[data]:
-        return True
-    else:
-        return False
-
-
-def command_handler(command_arr):
-    last_arr = command_arr[-1]
-    store_data = "store_data.txt"
-    target = command_arr[0]
-    hashmap_message_data = {}
-    hashmap_room_data = {}
-    hashmap_user_data = {}
-
-    ## create ##
-    if last_arr == "create":
-        room_name = command_arr[0]
-        admin = "admin"
-
-        # validation
-
-        # def store_chatroom_data(room_name):
-        with open("chatroom_data.txt", 'a') as chatroom_data:  # 'a' モードでファイルを開くことで追記モードになる
-            chatroom_data.write(f'\n{room_name}: {admin}: admin\n')
-        message = (
-            f"room {room_name} created you are "
-            f"admin. message from server.py"
-        )
-        hashmap_message_data["create"] = message
-
-    ## join ##
-    # [room,user_name,join]
-    elif last_arr == "join":
-        chatroom_name = command_arr[0]
-        user = command_arr[1]
-
-        if find_data(chatroom_name):
-            message = f"{user} successfuly joined {target}!"
-            hashmap_message_data["join"] = message
+        if ini_data_arr[1] not in rooms:
+            # 部屋のhashmapを作る
+            rooms[ini_data_arr[1]] = []
+            print(f"{user.name} connected from {addr}. created {room.name}")
+        # 既存のchatroomにjoin
         else:
-            print(f"room:{chatroom_name} doesnot exist")
+            print(f"{user.name} connected from {addr}. joined {room.name}")
 
-    ## send ##
-    elif last_arr == "send":
-        print("send")
-    ## help ##
-    elif last_arr == "help":
-        print("help!")
-    ## exit ##
-    elif last_arr == "exit":
-        print("exit")
-        client_socket.close()
-        server_socket.close()
-    ## 例外 ##
-    else:
-        print("tf?")
+        # 特定の部屋にユーザーを追加する
+        rooms[ini_data_arr[1]].append(user.name)
 
-    # {"create":message, "join":something,"help":something ~}
-    send_data(json.dumps(hashmap_message_data))
+        # ユーザー名を辞書に追加
+        # ここを全員格納するのではなく、room名と紐付けて格納する必要がある
+        clients[client_socket] = (user.name, room.name)
 
-####### command_handle ######################
+        ## 2回目のrecv##
+        while True:
+            # 多分送ってくる配列と取得する配列が違う??
+            message = client_socket.recv(1024).decode('utf-8')
+            print(message[0])
+            if not message[0]:
+                print("no message")
+                # クライアントが切断された場合
+                remove_client(client_socket)
+                break
+            else:
+
+                for client_sock, (username_client, roomname_client) in clients.items():
+                    # 同じ部屋かつ違うユーザー(ソケット)
+                    if roomname_client == room.name and client_sock != client_socket:
+                        message_data = {f"{user.name} said": message[0]}
+                        # message_data = {f"{user.name} said": message}
+                        # {"room": roomname_client, "username": username_client, "message": message})
+                        # json.dumps: 配列を文字列として取得
+                        # ってことはここも配列にしてjsonにして送る必要がある?
+                        client_sock.send(message_data.encode('utf-8'))
+
+                # クライアント自身にも送信する
+                message_data_to_myself = {f"You said": message}
+                # client_socket.send(message_data_to_myself.encode('utf-8'))
+                client_socket.send(message_data_to_myself.encode('utf-8'))
+
+    except:
+        print("error")
+        # remove_client(client_socket)
 
 
-######## received text##########
+def remove_client(client_socket):
+    if client_socket in clients:
+        # clients = []
+        # hashmapの値は2つ格納する事ができる
+        # clients[client_socket] = (user.name, ini_data_arr[1])
+        user, room = clients[client_socket]
+        print(f"{user} disconnected from room {room}")
+        del clients[client_socket]
+        # userをroomから削除する。
+        print(rooms[room])
+        rooms[room].remove(user)
+        if len(rooms[room]) == 0:
+            del rooms[room]
+
+
+# クライアントの接続を待ち受けるループ
+# handle_clientを呼び出しマルチスレッドで待ち受ける
 while True:
-    # receive
-    received_text = receive_data()
-    received_text_arr = json.loads(received_text)
-    print(received_text_arr)
-    # ここで関数に飛んで抜けてしまってない??
-    command_handler(received_text_arr)
-    if received_text_arr[-1] == "exit":
-        break
-######## received text##########
+    client_socket, addr = server.accept()
+    # clients[client_socket] = None  # 初期値としてNoneを設定
+    clients[client_socket] = ("", "")  # 初期値としてNoneを設定
 
+    # 各クライアントごとにスレッドを起動
+    client_thread = threading.Thread(
+        target=handle_client, args=(client_socket, addr))
+    client_thread.start()
 
-# close
-client_socket.close()
-server_socket.close()
-
-
-##### やる事####
 # 続き
-# joinのvalidationができた!!!
-# joinのlogicを作っていく。->まず、userを.txtのデータに追加する
+# これだと簡単すぎる。
+# チャットルームを作成、joinの機能を作って次に行く
+#########################################
+# {roomanema} :join #新規作成でも同じコマンド!
+# clientは一度に一つの部屋のみ参加できる[後]
+# % roomname messageでサーバに送信する
+# ↑送信する前に、そのclientがその部屋に参加しているか確認する
+##########################################
+
+# 続き
+# messageの表示(~said / /)の"/"を削除して自然な文章にする
+# ↓
+# データを送るときにjsonに変換する必要がある。その書き方が一貫していないからエラーが起こると思う。
+# ルール
+# データは文字列でも配列でも必ず配列のように書いてjsonにして送る。
+# 文字列の場合は["message" : message]というようにして送る
+# 取得するときは文字列の場合はvalue[0]で取得できる。
